dd967b3a0bc00df59998c2a43538a21e3089d6f2
Akhil Soni <akhilsoni2611@gmail.com>
2025-07-27 08:48:33 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuOpenButton = document.getElementById("menu-open-button");
+const menuCloseButton = document.getElementById("menu-close-button");
+
+
+menuOpenButton.addEventListener("click", ()=>{
+    // Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", ()=> menuOpenButton.click());
+
+  
